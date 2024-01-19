b3a20979bec5d8e8b264da83e07824febf52354d
유현근 <fxzzy@naver.com>
2024-01-19 17:08:18 +0900

    Create detail_page_class.py

---
+++ weather/detail_page_class.py
@@ -0,0 +1,35 @@
+
+class DetailPage:
+    def __init__(self, file_data_name, classification_system, providing_agency,
+                 department_name, department_phone_number, basis_of_possession,
+                 collection_method, update_period, next_registration_date,
+                 media_type, total_rows, extension, keywords,
+                 cumulative_downloads, download_shortcut, registration_date,
+                 modification_date, data_limit, provided_form, description,
+                 url, other_notes, cost_assessment, cost_basis_and_unit,
+                 usage_permission_range):
+        self.file_data_name = file_data_name
+        self.classification_system = classification_system
+        self.providing_agency = providing_agency
+        self.department_name = department_name
+        self.department_phone_number = department_phone_number
+        self.basis_of_possession = basis_of_possession
+        self.collection_method = collection_method
+        self.update_period = update_period
+        self.next_registration_date = next_registration_date
+        self.media_type = media_type
+        self.total_rows = total_rows
+        self.extension = extension
+        self.keywords = keywords
+        self.cumulative_downloads = cumulative_downloads
+        self.download_shortcut = download_shortcut
+        self.registration_date = registration_date
+        self.modification_date = modification_date
+        self.data_limit = data_limit
+        self.provided_form = provided_form
+        self.description = description
+        self.url = url
+        self.other_notes = other_notes
+        self.cost_assessment = cost_assessment
+        self.cost_basis_and_unit = cost_basis_and_unit
+        self.usage_permission_range = usage_permission_range
